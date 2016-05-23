698945bc4d71e9aad29b06fb0ae7b497ca38f84d
Ventero <ventero@ventero.de>
2016-05-23 20:39:15 +0200

    Remove trailing whitespace.

--- include/mrb_wrapper.hpp
+++ include/mrb_wrapper.hpp
@@ -17,42 +17,42 @@
 
 /**
  @brief Functions, templtes, and other goodies that let us use mruby with native types
- 
+
  @discussion
  There's a lot of template metaprogramming going on in this, and it may be slightly overwhelming.
  The important thing here is to remain calm. Put on some relaxing music.
  I know I had to when writing this stuff.
  */
 namespace NM::mrb {
-    
+
     /**
      @breif Exception for a bad conversion between an mrb type and a native type
      */
     class BadValueConersion : std::logic_error {
     public:
-        
+
         BadValueConersion(std::string &arg) : std::logic_error(arg) {};
         BadValueConersion(const char *w) : std::logic_error(w) {};
     };
-    
-    
+
+
     /**
      @brief function that mruby can use in method definitions and the like
      */
     typedef mrb_value (*callable)(mrb_state*, mrb_value);
-    
-    
+
+
     /**
      @brief template to create a destructor function for some type
      @see is_data_type_struct
      */
-    
+
     template<typename T>
     void destructor_value(mrb_state *mrb, void *self) {
         T* type = reinterpret_cast<T*>(self);
         delete type;
     }
-    
+
     /**
      @brief type traits relating to shared data between C++ and Mruby
      */
@@ -64,17 +64,17 @@ namespace NM::mrb {
         struct is_data_type_struct {
             constexpr static bool value = std::is_same<const struct mrb_data_type, T>::value;
         };
-        
-        
+
+
         /**
          @brief determine if a user-defined object can be shared with MRB
-         
+
          @discussion
-         
+
          This determines if some user-defined type can be shared with MRB.
          To determine sharability in the general case, you can use is_converable,
          which determines if a given type is sharable in any case.
-         
+
          A type is sharable with MRB if it has a static member of type "const struct mrb_data_type".
          This struct will determine the type's Ruby name, as well as a deleter function
          (which should almost always be a specialization of the `destructor_value` templte).
@@ -83,7 +83,7 @@ namespace NM::mrb {
         struct is_shared_native {
             constexpr static bool value = false;
         };
-        
+
         /**
          @cond 0
          Turning off documentation here for a second.
@@ -96,7 +96,7 @@ namespace NM::mrb {
         /**
          @endcond
          */
-        
+
         /**
          Type trait which determines if this is any type we can share with MRB, including primitive types
          such as mrb_float, mrb_bool, and so on.
@@ -110,10 +110,10 @@ namespace NM::mrb {
                                            || std::is_same<std::string, T>::value
                                            || std::is_same<const char*, T>::value);
         };
-        
-        
+
+
     }
-    
+
     /**
      @brief get the mrb_data-type struct used to share this type with mruby
      @see NM::mrb::traits::is_shared_native<T>
@@ -127,28 +127,28 @@ namespace NM::mrb {
             return &(std::remove_reference<rt>::type::mrb_type);
         }
     };
-    
-    
+
+
     template<typename T>
     inline typename std::enable_if<std::is_integral<T>::value && ! std::is_same<bool, T>::value, mrb_value>::type to_value(mrb_state *mrb, T i) {
         return mrb_fixnum_value(i);
     }
-    
+
     inline mrb_value to_value(mrb_state *mrb, bool b) {
         return mrb_bool_value(b);
     }
-    
-    
+
+
     template<typename T>
     inline typename std::enable_if<std::is_floating_point<T>::value, mrb_value>::type to_value(mrb_state *mrb, T i) {
         return mrb_float_value(mrb, i);
     }
-    
+
     // No need to template this, since it only works for strings!
     inline mrb_value to_value(mrb_state *mrb, std::string s) {
         return mrb_str_new(mrb, s.c_str(), s.length());
     }
-    
+
     /**
      Convert a user-defined type into an mruby object.
      Note that we can only convert copy-constructable objects at the current moment, and cannot handle pointers at all.
@@ -162,7 +162,7 @@ namespace NM::mrb {
         T *n = new T(obj);
         return mrb_obj_value(Data_Wrap_Struct(mrb, klass, type, n));
     }
-    
+
     template<typename T>
     typename std::enable_if<traits::is_shared_native<typename std::remove_pointer<T>::type>::value &&
     std::is_pointer<T>::value, mrb_value>::type to_value
@@ -171,11 +171,11 @@ namespace NM::mrb {
         struct RClass *klass = mrb_class_get(mrb, type->struct_name);
         return mrb_obj_value(Data_Wrap_Struct(mrb, klass, type, obj));
     }
-    
+
     inline std::string native_typename(mrb_value val) {
         return std::string{"Native type: "} + DATA_TYPE(val)->struct_name;
     }
-    
+
     inline std::string data_type_string(mrb_value val) {
         switch(mrb_type(val)) {
             case MRB_TT_TRUE:
@@ -199,8 +199,8 @@ namespace NM::mrb {
                 return "Something weird";
         }
     }
-    
-    
+
+
     template<typename T>
     typename std::enable_if<std::is_integral<T>::value, T>::type from_value(mrb_state *mrb, mrb_value val) {
         switch(mrb_type(val)) {
@@ -215,7 +215,7 @@ namespace NM::mrb {
                 throw BadValueConersion{q};
         }
     }
-    
+
     template<typename T>
     typename std::enable_if<std::is_floating_point<T>::value, T>::type from_value(mrb_state *mrb, mrb_value val) {
         switch(mrb_type(val)) {
@@ -230,17 +230,17 @@ namespace NM::mrb {
                 throw BadValueConersion("Value given was not a float");
         }
     }
-    
+
     template<typename T>
     typename std::enable_if<traits::is_shared_native<typename std::remove_reference<typename std::remove_pointer<T>::type>::type>::value>::type conversion_check(mrb_state *mrb, mrb_value val) {
         using Ti = typename std::remove_reference<typename std::remove_pointer<T>::type>::type;
         auto type = data_type<T>::value();
-        
+
         if(mrb_type(val) != MRB_TT_DATA) {
             auto q = std::string{"Expected a native type, recieved "} + data_type_string(val);
             throw BadValueConersion{q};
         }
-        
+
         if(DATA_TYPE(val) != type) {
             // Super expensive operation, but this just crashes the engine anyway, so who cares
             auto real_type = data_type_string(val);
@@ -249,8 +249,8 @@ namespace NM::mrb {
             throw BadValueConersion{q};
         }
     }
-    
-    
+
+
     template<typename T>
     typename std::enable_if<std::is_pointer<T>::value &&
     traits::is_shared_native<typename std::remove_pointer<T>::type>::value, T>::type
@@ -260,7 +260,7 @@ namespace NM::mrb {
         void *ptr = mrb_data_check_get_ptr(mrb, val, type);
         return static_cast<T>(ptr);
     }
-    
+
     template<typename T>
     typename std::enable_if<(! std::is_pointer<T>::value) &&
     traits::is_shared_native<T>::value, T>::type from_value(mrb_state *mrb, mrb_value val) {
@@ -274,12 +274,12 @@ namespace NM::mrb {
         // We than dereference this ptr to cause the copy constructor to be invoked
         return * static_cast<ptr>(p);
     }
-    
-    
-    
-    
 
-    
+
+
+
+
+
     /**
      mruby's mrb_get_args function takes a printf-style format string to specify argument types
      This struct allows us to get the format specifier for a single given type.
@@ -289,36 +289,36 @@ namespace NM::mrb {
     template<typename T, typename Extern = void>
     struct param_char {
     };
-    
+
     // o is for object
     template<typename T>
     struct param_char<T, typename std::enable_if<traits::is_shared_native<T>::value>::type> {
         constexpr static const auto value = 'o';
     };
-    
+
     // f is for float
     template<typename T>
     struct param_char<T, typename std::enable_if<std::is_floating_point<T>::value>::type> {
         constexpr static const auto value = 'f';
     };
-    
+
     // i is for integer
     template<typename T>
     struct param_char<T, typename std::enable_if<std::is_integral<T>::value && ! std::is_same<bool, T>::value>::type> {
         constexpr static auto value = 'i';
     };
-    
+
     // b is for bool
     template<typename T>
     struct param_char<T, typename std::enable_if<std::is_same<bool, T>::value>::type> {
         constexpr static auto value = 'b';
     };
-    
-    
-    
+
+
+
     /**
      Obtain the printf-style format specifier required by mrg_get_args.
-     
+
      We already defined templates to determine the proper character for arbitrary types, so this
      function returns the entire format string. It does so by folding over the types—
      Take the first type, get its format specifier character, add the string that specifies the rest of
@@ -328,64 +328,64 @@ namespace NM::mrb {
     struct param_format_string {
         static const char value[];
     };
-    
+
     template<typename ...Args>
     const char param_format_string<Args...>::value[] = {(param_char<typename std::remove_reference<Args>::type>::value)..., '\0'};
-    
+
     /**
      Defines a struct that assists in conversion of types from mruby types to C++ types.
      */
     template<typename T, typename U = void>
     struct conversion_helper {
     };
-    
-    
+
+
     // Specialization for floating point types
     template<typename T>
     struct conversion_helper<T, typename std::enable_if<std::is_floating_point<T>::value>::type> {
         mrb_float d;
         mrb_state *mrb;
-        
+
         operator T() {
             return static_cast<T>(d);
         }
-        
+
         void* to_ptr() {
             return (void *) &d;
         }
-        
+
     };
-    
+
     // Specialization for integral types
     template<typename T>
     struct conversion_helper<T,
     typename std::enable_if<std::is_integral<T>::value>::type> {
         mrb_int i;
         mrb_state *mrb;
-        
+
         operator T() {
             return static_cast<T>(i);
         }
-        
+
         void* to_ptr() {
             return (void *) &i;
         }
     };
-    
+
     // Specialization for strings
     template<> struct conversion_helper<std::string> {
         char *ptr;
         mrb_state *mrb;
-        
+
         operator std::string() {
             return ptr;
         }
-        
+
         void* to_ptr() {
             return (void *) &ptr;
         }
     };
-    
+
     /**
      Specialization of user-defined types.
      This works for reference types and value types, pointer types are handled seperately
@@ -393,12 +393,12 @@ namespace NM::mrb {
     template<typename T>
     struct conversion_helper<T,
     typename std::enable_if<traits::is_shared_native<typename std::remove_reference<T>::type>::value>::type> {
-        
+
         typedef typename std::remove_reference<T>::type contained_type;
-        
+
         mrb_value v;
         mrb_state *mrb;
-        
+
         operator T() {
             // mruby's mrb_data_check_get_ptr function does not raise on error, it simply returns NULL.
             // We want it to throw an exception, which mrb_data_get_ptr does.
@@ -406,38 +406,38 @@ namespace NM::mrb {
             contained_type *t = static_cast<contained_type*>(ptr);
             return *t;
         }
-        
+
         void* to_ptr() {
             return (void *) &v;
         }
     };
-    
+
     template<typename T>
     struct conversion_helper<T,
     typename std::enable_if< traits::is_shared_native<typename std::remove_pointer<T>::type>::value
     && std::is_pointer<T>::value>::type> {
         mrb_value v;
         mrb_state *mrb;
-        
+
         operator T() {
             void *ptr = mrb_data_get_ptr(mrb, v, data_type<typename std::remove_pointer<T>::type>::value());
             return static_cast<T>(ptr);
         }
-        
+
         void* to_ptr() {
             return &v;
         }
     };
-    
-    
+
+
     /**
      @brief template to help us bind native types to Ruby.
-     
+
      @discussion
-     
+
      This template class uses a variety of increasingly horrifying metaprogramming techniques to
      provide a clean(ish) user interface to share C++ classes to Ruby.
-     
+
      Let's say I have a type Vector, which has some methods.
      I can write a "binder function" which takes in an `mrb_state*`.
      I want to make that function bind a lot of methods into `mrb_state` so I can use my Vector type from ruby.
@@ -475,15 +475,15 @@ namespace NM::mrb {
     struct translator {
         static_assert(traits::is_shared_native<T>::value,
                       "can only translate shared native values");
-        
+
         static void makeClass(mrb_state *mrb) {
             mrb_define_class(mrb, data_type<T>::value()->struct_name, mrb->object_class);
         }
-        
+
         static struct RClass* getClass(mrb_state *mrb) {
             return mrb_class_get(mrb, data_type<T>::value()->struct_name);
         }
-        
+
         template<typename ...Args>
         struct constructor {
             static void bind(mrb_state *mrb) {
@@ -491,7 +491,7 @@ namespace NM::mrb {
                 mrb_define_class_method(mrb,
                                         getClass(mrb), "new", f, MRB_ARGS_REQ(sizeof...(Args)));
             }
-            
+
         private:
             static mrb_value val(mrb_state *mrb, mrb_value self) {
                 std::string format = param_format_string<Args...>::value;
@@ -501,19 +501,19 @@ namespace NM::mrb {
                 T* constructed = make_call(t, std::index_sequence_for<Args...>{});
                 return mrb_obj_value(Data_Wrap_Struct(mrb, getClass(mrb), data_type<T>::value(), (void *) constructed));
             }
-            
+
             template<class Tuple, std::size_t... indexes>
             static T* make_call(Tuple &t, std::index_sequence<indexes...>) {
                 return new T(std::get<indexes>(t)...);
             }
         };
-        
+
         template<typename Ret, typename ...Args>
         struct method {
             typedef Ret(T::*funcType)(Args...);
             typedef Ret(T::*constFuncType)(Args...) const;
-            
-            
+
+
             template<funcType func>
             struct binder{
                 static void bind(mrb_state *mrb, std::string name) {
@@ -521,7 +521,7 @@ namespace NM::mrb {
                     mrb_define_method(mrb,
                                       getClass(mrb), name.c_str(), f, sizeof...(Args));
                 }
-                
+
             private:
                 static mrb_value method(mrb_state *mrb, mrb_value self) {
                     std::string format = param_format_string<Args...>::value;
@@ -533,13 +533,13 @@ namespace NM::mrb {
                     Ret re = make_call(s, func, t, std::index_sequence_for<Args...>{});
                     return to_value(mrb, re);
                 }
-                
+
                 template<class Tuple, std::size_t... indexes>
                 static Ret make_call(T *self, funcType f, Tuple &t, std::index_sequence<indexes...>) {
                     return (self->*f)(std::get<indexes>(t)...);
                 }
             };
-            
+
             template<constFuncType func>
             struct const_binder {
                 static void bind(mrb_state *mrb, std::string name) {
@@ -547,7 +547,7 @@ namespace NM::mrb {
                     mrb_define_method(mrb,
                                       getClass(mrb), name.c_str(), f, sizeof...(Args));
                 }
-                
+
             private:
                 static mrb_value method(mrb_state *mrb, mrb_value self) {
                     std::string format = param_format_string<Args...>::value;
@@ -559,7 +559,7 @@ namespace NM::mrb {
                     Ret re = make_call(s, func, t, std::index_sequence_for<Args...>{});
                     return to_value(mrb, re);
                 }
-                
+
                 /**
                  Use parameter pack expansion and an index sequence paramater to call the function
                  */
@@ -567,16 +567,16 @@ namespace NM::mrb {
                 static Ret make_call(T *self, constFuncType f, Tuple &t, std::index_sequence<indexes...>) {
                     return (self->*f)(std::get<indexes>(t)...);
                 }
-                
+
             };
-            
+
         };
-        
+
     protected:
         /**
          Given a tuple of mrb_conversion_helpers, sets each one of their mrb members to the current mrb_state.
          We need this to do some value conversions.
-         
+
          Uses that same nasty static paramater pack shit the other things use
          */
         template<class Tuple, std::size_t... indexes>
@@ -587,7 +587,7 @@ namespace NM::mrb {
             // C++ parameter pack syntax: It's... It's not very good
             (void) swallow{0, (void (std::get<indexes>(t).mrb = mrb), 0)...};
         }
-        
+
         /**
          Nasty paramater packs shit to properly expand this into varargs. So gross.
          */
@@ -599,7 +599,7 @@ namespace NM::mrb {
             mrb_get_args(mrb, format.c_str(), (std::get<indexes>(t).to_ptr())...);
         }
     };
-    
+
 }
 
 #endif /* mrb_wrapper_h */
