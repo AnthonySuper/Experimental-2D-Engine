b62fa310330f3f27b83dbac9970961a145d21f33
Ventero <ventero@ventero.de>
2016-05-23 21:38:10 +0200

    Add some more tests.

--- test/mrb_wrapper.cpp
+++ test/mrb_wrapper.cpp
@@ -15,3 +15,36 @@ TEST_CASE("param_char", "[mrb_wrapper]") {
 		REQUIRE((param_char<NM::Vector>::value == 'o'));
 	}
 }
+
+TEST_CASE("param_format_string", "[mrb_wrapper]") {
+	REQUIRE((std::string("fibo") == param_format_string<float, int, bool, NM::Vector>::value));
+	REQUIRE((std::string("") == param_format_string<>::value));
+}
+
+TEST_CASE("to_value", "[mrb_wrapper]") {
+    mrb_state *mrb = mrb_open();
+
+	SECTION("positive integer") {
+		mrb_value v = to_value(mrb, 1);
+		REQUIRE(data_type_string(v) == "fixnum");
+		REQUIRE(from_value<int>(mrb, v) == 1);
+	}
+
+	SECTION("negative integer") {
+		mrb_value v = to_value(mrb, -15);
+		REQUIRE(data_type_string(v) == "fixnum");
+		REQUIRE(from_value<int>(mrb, v) == -15);
+	}
+
+	SECTION("positive short") {
+		mrb_value v = to_value(mrb, static_cast<short>(123));
+		REQUIRE(data_type_string(v) == "fixnum");
+		REQUIRE(from_value<short>(mrb, v) == 123);
+	}
+
+	SECTION("negative short") {
+		mrb_value v = to_value(mrb, static_cast<short>(-123));
+		REQUIRE(data_type_string(v) == "fixnum");
+		REQUIRE(from_value<short>(mrb, v) == -123);
+	}
+}
