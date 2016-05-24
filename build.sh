d56b7112c0f7d51cd95bf9fa2d23db5a51bf74d9
Ventero <ventero@ventero.de>
2016-05-24 23:20:19 +0200

    Add Linux to Travis CI.

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ex
+
+# Compile mruby first.
+(cd mruby && make -j2)
+
+# Override CXX if specified by the CI environment.
+if [ -n "$COMPILER" ]; then
+	export CXX="$COMPILER"
+fi
+
+# Generate our build directory.
+mkdir -p build
+cd build
+
+# Build and run everything.
+if [ "$TRAVIS_OS_NAME" = osx ]; then
+	cmake -G Xcode ..
+	xcodebuild
+	Debug/catch_test
+else
+	cmake ..
+	make VERBOSE=1 -j2 all
+	./catch_test
+fi
